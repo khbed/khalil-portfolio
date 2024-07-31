67b391c33840a89914b5f25fdc20038275127141
khbed <97263154+khbed@users.noreply.github.com>
2024-07-30 23:58:28 -0400

    Centered project card elements

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -8,7 +8,7 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title style={{paddingBottom: "0.5em", minHeight: props.needsNewLine? "3em" : "2em"}}>{props.title}</Card.Title>
+        <Card.Title style={{textAlign: "center", paddingBottom: "0.5em", minHeight: props.needsNewLine? "3em" : "2em"}}>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "left", fontSize: "0.9em", minHeight: props.needsNewLine? "10em" : "8em"}}>
           <p>
           {props.description}
@@ -16,7 +16,7 @@ function ProjectCards(props) {
           
         </Card.Text>
         
-        <div>
+        <div style={{display: "flex", justifyContent: "center"}}>
           <Button style={{position: "relative"}} variant="primary" href={props.ghLink} target="_blank">
             <BsGithub /> &nbsp;
             GitHub
